3513b6e6927327000b88b9fb210a3245ee2ef423
nathanjh-28 <nathan@nathanjharris.com>
2021-02-21 16:57:25 -0700

    checkbox examples complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Button from '@material-ui/core/Button'
+import ButtonGroup from '@material-ui/core/ButtonGroup'
 import SaveIcon from '@material-ui/icons/Save'
+import DeleteIcon from '@material-ui/icons/Delete'
+import Checkbox from '@material-ui/core/Checkbox'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+
+function CheckboxExample() {
+  const [checked, setChecked] = React.useState(true)
+  return (
+    <FormControlLabel
+      control={<Checkbox
+        checked={checked}
+        // make the checkbox a save icon when unchecked
+        icon={<SaveIcon />}
+        // make the checkbox a color icon when checked
+        checkedIcon={<SaveIcon color="secondary" />}
+
+        onChange={(e) => setChecked(e.target.checked)}
+        color="secondary"
+        inputProps={{ 'aria-label': 'secondary checkbox' }}
+      />}
+      label="Example Checkbox"
+    />
+
+
+
+
+  )
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Button
-          startIcon={<SaveIcon />}
-          // endIcon={<SaveIcon />}
-          // onClick={() => alert('hello')}
-          size="large"
-          // style={{fontSize: 48}}
-          href='#' variant="contained" color="secondary"
-        >
-          Hello World
+        <CheckboxExample />
+        <ButtonGroup color="primary">
+
+          <Button
+            startIcon={<SaveIcon />}
+            // endIcon={<SaveIcon />}
+            // onClick={() => alert('hello')}
+            // size="large"
+            // style={{fontSize: 48}}
+            href='#'
+            variant="contained"
+            color="primary"
+          >
+            Save
         </Button>
-        <img src={logo} className="App-logo" alt="logo" />
+          <Button
+            startIcon={<DeleteIcon />}
+            size="large"
+            color="secondary"
+            variant="contained"
+
+          >
+            Discard
+        </Button>
+        </ButtonGroup>
+
 
 
       </header>
